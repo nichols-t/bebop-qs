c374864335792579d349539c4d1c754d4fd994da
Ty Nichols <tynichols0@protonmail.com>
2026-08-09 13:38:18 -0400

    Updated comments in hyprland config

--- quickshell.lua
+++ quickshell.lua
@@ -1,26 +1,23 @@
--- qs -p shell.qml ipc --any-display call shutdownMenu showShutdownMenu
+-- NEED TO:
+-- ln -s ~/.config/quickshell/quickshell.lua ~/.config/hypr/quickshell.lua
+-- and then require it from your hyprland config
 
--- hyprctl -j monitors | jq '.[] | select(.focused == true) | .name'
+-- Keybind for the shutdown/power menu
 hl.bind(
     "SUPER + CTRL + Q",
-    -- Note here that we have to do this jq stuff because the IPC is per-monitor
-    -- which is kinda awkward. Consequence of variants; there is probably a better
-    -- way to do that but going to leave it for now
     hl.dsp.exec_cmd("qs ipc call root showShutdownMenu")
 )
 
+-- Keybind for the app launcher
 hl.bind(
   "SUPER + SPACE",
   hl.dsp.exec_cmd("qs ipc call root showAppLauncher")
 )
 
-
+-- Animation rule for the shutdown menu
 hl.layer_rule({
   match = {
     class = "shutdownMenu"
   },
   animation = "fade"
 })
-
--- NEED TO:
--- ln -s ~/.config/quickshell/quickshell.lua ~/.config/hypr/quickshell.lua
